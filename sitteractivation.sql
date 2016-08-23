2e300c1c2c76d8c4661d7e3d868b034533d27a37
Ting Wong <tingwong@users.noreply.github.com>
2016-08-23 14:57:21 -0700

    Create sitteractivation.sql

---
+++ sitteractivation.sql
@@ -0,0 +1,29 @@
+SELECT au.email AS 'Email Address',
+au.first_name AS 'First Name',
+CASE WHEN cc.id IS NOT NULL THEN 1 ELSE 0 END AS 'sitter_had_convo',
+CASE WHEN cc2.provider_id IS NOT NULL THEN 1 ELSE 0 END AS 'sitter_had_booking',
+CASE WHEN cbsa_code IN (11980, 19740, 12420, 29820, 26420, 42660, 19100, 31100, 37100, 41740, 31080, 40140, 42060, 38900, 36740, 27260, 45300, 46060, 38060,12420, 42660, 19740, 29820) THEN 1 ELSE 0 END AS 'premier_flag'
+
+FROM auth_user au
+JOIN people_person p ON p.user_id = au.id
+JOIN people_personlocation ppl ON ppl.person_id = p.id
+JOIN metrics_combinedstatisticalarea mcsa ON mcsa.zip_code = ppl.zip
+LEFT JOIN conversations_conversation cc ON cc.provider_id = p.id
+
+JOIN (SELECT provider_id, 
+	  MIN(searchable_date) AS 'min_search_date' 
+      FROM services_service 
+      WHERE active = 1
+      AND searchable = 1
+      GROUP BY provider_id) serv ON serv.provider_id = p.id
+
+LEFT JOIN (SELECT provider_id
+FROM conversations_conversation 
+WHERE has_stay = 1
+GROUP BY provider_id) cc2 ON cc2.provider_id = cc.provider_id
+
+WHERE serv.min_search_date > CURDATE() - INTERVAL 60 DAY
+
+GROUP BY au.email
+
+HAVING sitter_had_convo = 0 OR sitter_had_booking = 0
